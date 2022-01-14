60fa3e67f6b3f6af0ff2d3a246a9f090426ab10e
Carolina Bastos <carol1999b@gmail.com>
2022-01-14 12:11:14 +0000

    add moreanimations
    Made by Francisco

--- VR_Blood/Assets/Scripts/Tp.cs
+++ VR_Blood/Assets/Scripts/Tp.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Tp : MonoBehaviour
 {
     [SerializeField] private GameObject rig;
+    [SerializeField] private GameObject tpobject;
+    [SerializeField] private Animator fadeout;
+
     [SerializeField] private float x;
     [SerializeField] private float y;
     [SerializeField] private float z;
 
     public void TpCharacter()
     {
-        rig.transform.position = new Vector3(x,y,z);
+        rig.transform.position = tpobject.transform.position;
     }
 
+    public void FadeOut()
+    {
+        fadeout.Play("FadeOut");
+    }
 }
